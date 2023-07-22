159caaaa2636310a16b1ee943f763e67c41d6660
chillipeppersmoothy <adityashenoy11@gmail.com>
2023-07-22 09:51:20 +0530

    04

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,7 +2,7 @@ const products_DB = require('../models/product')
 
 const getAllProducts = async (req, res) => {
         //query params structure
-        const { featured, rating, company, name } = req.query;
+        const { featured, rating, company, name, sort, fields, numericFilters } = req.query;
         const queryObject = {}
     
         if (featured) {
@@ -20,16 +20,63 @@ const getAllProducts = async (req, res) => {
         if (name) {
             queryObject.name = { $regex: name, $options: 'i'};
         }
-        console.log(queryObject);
 
-    const products = await products_DB.find(queryObject);
+        if(numericFilters) {
+            // set symbols to letters
+            const operatorMap = {
+                '>': '$gt',
+                '>=': '$gte',
+                '=': '$eq',
+                '<': '$lt',
+                '<=': '$lte'
+            }
+            // add regex to find values
+            const regEx = /\b(<|>|>=|<=|=)\b/g
+            // replace symbols with mapped values
+            let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+            // options for filters
+            const options = ['price','rating'];
+
+            filters = filters.split(',').forEach((item) => {
+                const [field, operator, value] = item.split('-');
+                if(options.includes(field)) {
+                    queryObject[field] = { [operator]: Number(value) }
+                }
+            });
+            console.log(queryObject)
+        }
+
+        let result = products_DB.find(queryObject);
+
+        //sort
+        if(sort) {
+            const sortList = sort.split(',').join(' ');
+            result = result.sort(sortList);
+        } else {
+            result = result.sort('createdAt');
+        }
+
+        //display fields
+        if(fields) {
+            const fieldList = fields.split(',').join(' ');
+            //.select is used to display only those fields mentioned in it.
+            result = result.select(fieldList);
+        }
+
+        //items limit and page
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page-1) * limit;
+
+        result = result.limit(limit).skip(skip);
+
+    const products = await result;
     await res.status(200).json({ items: products.length, products: products });
 }
 
 const getAllStaticProducts = async (req, res) => {
 
-
-    const products = await products_DB.find().sort('-name');
+    const products = await products_DB.find({price: { $gt: 30 }}).sort('price').limit(10).select('name price');
     await res.status(200).json({ items: products.length, products: products });
 }
 
--- 04-store-api/starter/models/product.js
+++ 04-store-api/starter/models/product.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
     company: {
         type: String, enum: {
             values: ['ikea', 'liddy', 'caressa', 'marcos'],
-            message: `{VALUE} is ot supported`
+            message: `{VALUE} is not supported`
         }
     }
 });
